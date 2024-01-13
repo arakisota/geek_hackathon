29c2ccc27bf037630a7a172541db0fa7c13abbd4
Keita Nakajima <confi2100@gmail.com>
2024-01-13 17:13:19 +0900

    usermodelの定義修正

--- server/model/user.go
+++ server/model/user.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type User struct {
-	UserId    uint      `json:"id" gorm:"primaryKey;unique"`
+	UserId    string    `json:"id" gorm:"primaryKey;unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
-	UserId    uint      `json:"id" gorm:"primaryKey;unique"`
+	UserId    string    `json:"id" gorm:"primaryKey;unique"`
 }
