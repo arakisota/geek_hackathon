7f6c1697182e5334dfc7d0c0012e3fa5f5b08cb8
Keita Nakajima <confi2100@gmail.com>
2024-01-13 18:56:24 +0900

    UserIdのkey値修正

--- server/model/user.go
+++ server/model/user.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type User struct {
-	UserId    string    `json:"id" gorm:"primaryKey;unique"`
+	UserId    string    `json:"user_id" gorm:"primaryKey;unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
-	UserId    string    `json:"id" gorm:"primaryKey;unique"`
+	UserId    string    `json:"user_id" gorm:"primaryKey;unique"`
 }
