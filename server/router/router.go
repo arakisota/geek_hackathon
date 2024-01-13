0604ccf5c0cfe80210b567b4130c88c3b3c8f9de
Keita Nakajima <confi2100@gmail.com>
2024-01-13 18:38:29 +0900

    account関連のエンドポイントの作成

---
+++ server/router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"server/controller"
+
+	"github.com/labstack/echo/v4"
+)
+
+func NewRouter(uc controller.IUserController) *echo.Echo {
+	e := echo.New()
+	e.POST("/signup", uc.SignUp)
+	e.POST("/login", uc.LogIn)
+	e.POST("/logout", uc.LogOut)
+	return e
+}
