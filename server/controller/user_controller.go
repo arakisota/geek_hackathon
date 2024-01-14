dbd1b0b50dd4c48444cc0226ab30a8487c69b9e7
Keita Nakajima <confi2100@gmail.com>
2024-01-14 12:27:30 +0900

    ミドルウェアの追加(CORS,CSRF)

--- server/controller/user_controller.go
+++ server/controller/user_controller.go
@@ -14,6 +14,7 @@ type IUserController interface {
 	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
+	CsrfToken(c echo.Context) error
 }
 
 type userController struct {
@@ -70,4 +71,11 @@ func (uc *userController) LogOut(c echo.Context) error {
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
 	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) CsrfToken(c echo.Context) error {
+	token := c.Get("csrf").(string)
+	return c.JSON(http.StatusOK, echo.Map{
+		"csrf_token": token,
+	})
 }
