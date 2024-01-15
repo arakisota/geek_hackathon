2042e07a1759d2b75e1e2ced12269eed4c787c38
Keita Nakajima <confi2100@gmail.com>
2024-01-15 16:54:13 +0900

    /stationのリクエスト・レスポンス形式の定義

--- server/model/station.go
+++ server/model/station.go
@@ -1,10 +1,20 @@
 package model
 
 type TransportRecord struct {
-    InformationId uint   `gorm:"primaryKey;autoIncrement"`
-    Departure     string
-    Destination   string
-    Time          uint
-    Count         uint
-    Fare          uint
+	InformationId uint `gorm:"primaryKey;autoIncrement"`
+	Departure     string
+	Destination   string
+	Time          uint
+	Count         uint
+	Fare          uint
+}
+
+type StationsRequest struct {
+	Departures []string `json:"departures"`
+}
+
+type StationsResponse struct {
+	FastestStation         string `json:"fastest_station"`
+	FewestTrasitionStation string `json:"fewest_trasition_station"`
+	CheapestStation        string `json:"cheapest_station"`
 }
