245aea062cf34291c5a6afbef4564c7b5cc9f2cd
arakisota <soutaraki0126@gmail.com>
2024-01-19 15:46:18 +0900

    クライアントのレスポンスの構造体を変更し、ホットペッパーグルメAPIからのレスポンスデータの定義を削除

--- server/model/restaurant.go
+++ server/model/restaurant.go
@@ -1,13 +1,22 @@
 package model
 
 import (
-	"net/url"
-	"strconv"
 	"time"
 )
 
 // データの構造を定義
 // 具体的には、クライアントからのリクエストデータ、ホットペッパーグルメAPIへのリクエストデータ、ホットペッパーグルメAPIからのレスポンスデータ、クライアントへのレスポンスデータの各構造体を定義する
+
+type StationCoordinates struct {
+	Name      string
+	Latitude  float64
+	Longitude float64
+}
+
+type Genre struct {
+	Catch string `json:"catch"`
+	Name  string `json:"name"`
+}
 
 // クライアントからのリクエストデータを定義
 type ClientRequest struct {
@@ -19,56 +28,37 @@ type ClientRequest struct {
 
 // ホットペッパーグルメAPIへのリクエストデータを定義
 type HotpepperRequest struct {
-	Name    string  `json:"name"`
-	Keyword string  `json:"keyword"`
-	Lat     float64 `json:"lat"`
-	Lng     float64 `json:"lng"`
-	Range   int     `json:"range"`
-	Budget  string  `json:"budget"`
-	Order   int     `json:"order"`
-	Format  string  `json:"format"`
-}
-
-func (hr *HotpepperRequest) ToURLValues() url.Values {
-	values := url.Values{}
-	values.Add("name", hr.Name)
-	values.Add("keyword", hr.Keyword)
-	values.Add("lat", strconv.FormatFloat(hr.Lat, 'f', -1, 64))
-	values.Add("lng", strconv.FormatFloat(hr.Lng, 'f', -1, 64))
-	values.Add("range", strconv.Itoa(hr.Range))
-	values.Add("budget", hr.Budget)
-	values.Add("order", strconv.Itoa(hr.Order))
-	values.Add("format", hr.Format)
-
-	return values
+	Name []string  `json:"name"`
+	Lat  []float64 `json:"lat"`
+	Lng  []float64 `json:"lng"`
 }
 
 // ホットペッパーグルメAPIからのレスポンスデータを定義
 type HotpepperResponse struct {
-	Name         string    `json:"name"`
-	Access       string    `json:"access"`
-	Rating       float64   `json:"rating"`
-	Price        float64   `json:"price"`
-	Availability bool      `json:"availability"`
-	OpenTime     time.Time `json:"open_time"`
-	Genre        string    `json:"genre"`
-	CouponUrls   string    `json:"coupon_urls"`
+	Name       string  `json:"name"`
+	Address    string  `json:"address"`
+	Access     string  `json:"access"`
+	Latitude   float64 `json:"latitude"`
+	Longitude  float64 `json:"longitude"`
+	Budget     string  `json:"budget"`
+	Open       string  `json:"open"`
+	Genre      Genre   `json:"genre"`
+	CouponUrls string  `json:"coupon_urls"`
+}
+
+type Station struct {
+	Name       string  `json:"name"`
+	Address    string  `json:"address"`
+	Access     string  `json:"access"`
+	Latitude   float64 `json:"lat"`
+	Longitude  float64 `json:"lng"`
+	Budget     string  `json:"budget"`
+	Open       string  `json:"open"`
+	Genre      Genre   `json:"genre"`
+	CouponUrls string  `json:"coupon_urls"`
 }
 
 // クライアントへのレスポンスデータを定義
 type ClientResponse struct {
-	Name         string    `json:"name"`
-	Access       string    `json:"access"`
-	Rating       float64   `json:"rating"`
-	Price        float64   `json:"price"`
-	Availability bool      `json:"availability"`
-	OpenTime     time.Time `json:"open_time"`
-	Genre        string    `json:"genre"`
-	CouponUrls   string    `json:"coupon_urls"`
-}
-
-type StationCoordinates struct {
-	Name      string
-	Latitude  float64
-	Longitude float64
+	Stations []Station `json:"stations"`
 }
