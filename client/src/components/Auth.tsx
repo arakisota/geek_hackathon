4a7256ac296ed64fecf0069e5b08c09045bdd96a
arakisota <soutaraki0126@gmail.com>
2024-01-21 14:48:57 +0900

    ログインページにロゴの写真を添付

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -1,66 +1,50 @@
 import { FC, useState, FormEvent } from 'react'
-import { ArrowPathIcon, CheckBadgeIcon } from '@heroicons/react/24/solid'
+import {
+  ArrowPathIcon,
+} from '@heroicons/react/24/solid'
 import { useMutateAuth, MutateAuthProps } from '../hooks/useMutateAuth'
+import togatherLogo from '../assets/logo_cream.png';
 
 export type AuthProps = {
   setIsLogined: (state: boolean) => void
 }
 
-export const Auth: FC<AuthProps> = (props) => {
-  const { setIsLogined } = props
-
+export const Auth: FC<AuthProps> = ({ setIsLogined }) => {
   const [userId, setUserId] = useState('')
   const [pw, setPw] = useState('')
   const [isLogin, setIsLogin] = useState(true)
-  const { loginMutation, registerMutation } = useMutateAuth({
-    setIsLogined,
-  } as MutateAuthProps)
+  const { loginMutation, registerMutation } = useMutateAuth({ setIsLogined } as MutateAuthProps)
 
   const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (isLogin) {
-      loginMutation.mutate({
-        user_id: userId,
-        password: pw,
-      })
+      loginMutation.mutate({ user_id: userId, password: pw })
     } else {
-      await registerMutation
-        .mutateAsync({
-          user_id: userId,
-          password: pw,
-        })
-        .then(() =>
-          loginMutation.mutate({
-            user_id: userId,
-            password: pw,
-          })
-        )
+      await registerMutation.mutateAsync({ user_id: userId, password: pw })
+        .then(() => loginMutation.mutate({ user_id: userId, password: pw }))
     }
   }
+
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
-      <div className="flex items-center">
-        <CheckBadgeIcon className="h-8 w-8 mr-2 text-blue-500" />
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono " style={{backgroundColor: '#F5F5F5'}}>
+      <img src={togatherLogo} alt="ToGather Logo" width="800" height="800"/>
 
-        <span className="text-center text-3xl font-extrabold">Home</span>
-      </div>
-
-      <h2 className="my-6">{isLogin ? 'Login' : 'Create a new account'}</h2>
+      <h2 className="my-6 text-2xl">{isLogin ? 'Login' : 'Create a new account'}</h2>
       <form onSubmit={submitAuthHandler}>
-        <div>
+        <div className="mb-4">
           <input
-            className="mb-3 px-3 text-sm py-2 border border-gray-300"
+            className="mb-4 px-4 text-lg py-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black-500 transition-all w-80"
             name="user_id"
-            type="user_id"
+            type="text"
             autoFocus
             placeholder="UserID"
             onChange={(e) => setUserId(e.target.value)}
             value={userId}
           />
         </div>
-        <div>
+        <div className="mb-4">
           <input
-            className="mb-3 px-3 text-sm py-2 border border-gray-300"
+            className="mb-4 px-4 text-lg py-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black-500 transition-all w-80"
             name="password"
             type="password"
             placeholder="Password"
@@ -68,9 +52,9 @@ export const Auth: FC<AuthProps> = (props) => {
             value={pw}
           />
         </div>
-        <div className="flex justify-center my-2">
+        <div className="flex justify-center my-4">
           <button
-            className="disabled:opacity-40 py-2 px-4 rounded text-white bg-indigo-600"
+            className="disabled:opacity-40 disabled:cursor-not-allowed py-3 px-8 rounded text-lg text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
             disabled={!userId || !pw}
             type="submit"
           >
@@ -80,7 +64,7 @@ export const Auth: FC<AuthProps> = (props) => {
       </form>
       <ArrowPathIcon
         onClick={() => setIsLogin(!isLogin)}
-        className="h-6 w-6 my-2 text-blue-500 cursor-pointer"
+        className="h-8 w-8 my-2 text-blue-500 cursor-pointer hover:text-blue-600 transition-all"
       />
     </div>
   )
