cac2737591016243175f3d3dd1e6d7c4e3a031b5
Keita Nakajima <confi2100@gmail.com>
2024-02-27 19:00:06 +0900

    サイドバーの実装

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -12,9 +12,13 @@ import { mapStyle } from '../types/mapStyle'
 import { Form } from './Form'
 import { Plan } from './Plan'
 import { useMutateAuth, MutateAuthProps } from '../hooks/useMutateAuth'
-import { FaChevronDown, FaChevronRight } from 'react-icons/fa'
+import { FaWpforms } from 'react-icons/fa'
+import { IoChatboxEllipsesOutline } from 'react-icons/io5'
+import { TbLogout, TbSend } from 'react-icons/tb'
+import togather from '../assets/toGather.png'
 
 type MapProps = {
+  userId: string
   roomId: string
   setIsLogined: (state: boolean) => void
   ws: WebSocket | null
@@ -30,7 +34,7 @@ const defaultCenter = { lat: 35.6895, lng: 139.6917 }
 
 export const Map: React.FC<MapProps> = (props) => {
   const mapRef = useRef<google.maps.Map>()
-  const { roomId, setIsLogined, ws, setWs } = props
+  const { userId, roomId, setIsLogined, ws, setWs } = props
   const { logoutMutation } = useMutateAuth({
     roomId,
     setIsLogined,
@@ -46,6 +50,7 @@ export const Map: React.FC<MapProps> = (props) => {
   const token = localStorage.getItem('token')
   const [messages, setMessages] = useState<string[]>([])
   const [input, setInput] = useState('')
+  const [isChatVisible, setIsChatVisible] = useState<boolean>(false)
   useEffect(() => {
     const websocket = new WebSocket(
       `ws://localhost:8080/ws?token=${token}&room_id=${roomId}`
@@ -115,7 +120,6 @@ export const Map: React.FC<MapProps> = (props) => {
   // -------------------------- Form --------------------------
   // フォーム上の操作、表示
   const [isFormVisible, setIsFormVisible] = useState(true)
-  const toggleFormVisibility = () => setIsFormVisible(!isFormVisible)
   const [showForm, setShowForm] = useState(true)
   const handleFormSubmit = () => setShowForm(false)
   const handleBackToForm = () => setShowForm(true)
@@ -184,45 +188,108 @@ export const Map: React.FC<MapProps> = (props) => {
 
   return (
     <>
-      <div className="p-4">
-        <div className="mb-4">
-          {messages.map((message, index) => (
-            <div key={index}>{message}</div>
-          ))}
+      <div className="fixed inset-0 bg-gray-600 w-16 z-30">
+        <div className="w-16 h-16 flex items-center justify-center">
+          <img src={togather} className="w-12 h-10" />
         </div>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          className="border p-2 mr-2"
-        />
-        <button onClick={sendMessage} className="bg-blue-500 text-white p-2">
-          Send
+        <button
+          className={`w-16 h-16 flex items-center justify-center ${
+            isFormVisible ? 'text-black bg-white' : 'text-white'
+          }`}
+          onClick={() => setIsFormVisible(!isFormVisible)}
+        >
+          <FaWpforms size={24} />
         </button>
+        <button
+          className={`w-16 h-16 flex items-center justify-center text-white ${
+            isChatVisible ? 'bg-black bg-opacity-50' : ''
+          }`}
+          onClick={() => setIsChatVisible(!isChatVisible)}
+        >
+          <IoChatboxEllipsesOutline size={24} />
+        </button>
+        <div className="self-end">
+          <button
+            className="w-16 h-16 rounded-full flex items-center justify-center text-white"
+            onClick={logout}
+          >
+            <TbLogout size={24} />
+          </button>
+        </div>
       </div>
-      <div className="relative">
-        {showForm ? (
-          <>
-            <div className="absolute top-0 left-0 z-10 p-4 ml-4 mt-4 max-w-xl bg-white rounded shadow-lg">
-              <button onClick={toggleFormVisibility}>
-                {isFormVisible ? <FaChevronDown /> : <FaChevronRight />}
-              </button>
-              <div style={{ display: isFormVisible ? 'block' : 'none' }}>
-                <Form
-                  onStationSelect={handleStationSelect}
-                  setRestaurantsRequest={setRestaurantsRequest}
-                  setRoutesRequest={setRoutesRequest}
-                  onSubmit={handleFormSubmit}
-                />
+
+      <div style={{ display: isChatVisible ? 'block' : 'none' }}>
+        <div className="fixed inset-0 bg-black bg-opacity-50 max-w-lg flex justify-center pl-20 pr-4 py-4 z-20 overflow-hidden">
+          <div className="w-full flex flex-col">
+            <div className="p-4 flex-1 overflow-auto">
+              <div className="space-y-2 w-full">
+                {messages.map((message, index) => {
+                  const data = JSON.parse(message)
+                  let baseStyle = 'px-4 py-2 rounded-t-2xl break-words w-fit'
+                  let justifyContent =
+                    data.userId === userId ? 'justify-end' : 'justify-start'
+
+                  if (data.type === 'login' || data.type === 'logout') {
+                    return (
+                      <div
+                        key={index}
+                        className="text-center text-white text-sm"
+                      >
+                        {data.message}
+                      </div>
+                    )
+                  } else {
+                    return (
+                      <div key={index} className={`flex ${justifyContent}`}>
+                        <div
+                          className={`${baseStyle} ${
+                            data.userId === userId
+                              ? 'bg-green-300 rounded-bl-2xl'
+                              : 'bg-gray-300 rounded-br-2xl'
+                          }`}
+                        >
+                          {data.message}
+                        </div>
+                      </div>
+                    )
+                  }
+                })}
               </div>
             </div>
-          </>
-        ) : (
-          <div className="absolute top-0 left-0 z-10 p-4 ml-4 mt-4 max-w-xl bg-white rounded shadow-lg max-h-[95%] overflow-y-auto">
-            <button onClick={toggleFormVisibility}>
-              {isFormVisible ? <FaChevronDown /> : <FaChevronRight />}
-            </button>
-            <div style={{ display: isFormVisible ? 'block' : 'none' }}>
+            <hr className="my-4 border-gray-300" />
+            <div className="px-4 pb-4">
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  className="flex-1 border rounded-lg p-2 mr-2"
+                />
+                <button
+                  onClick={sendMessage}
+                  className="bg-blue-500 text-white rounded-lg p-2"
+                >
+                  <TbSend size={26} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative">
+        <div style={{ display: isFormVisible ? 'block' : 'none' }}>
+          {showForm ? (
+            <div className="absolute top-0 left-0 z-10 p-4 ml-16 mt-4 max-w-xl bg-white rounded shadow-lg">
+              <Form
+                onStationSelect={handleStationSelect}
+                setRestaurantsRequest={setRestaurantsRequest}
+                setRoutesRequest={setRoutesRequest}
+                onSubmit={handleFormSubmit}
+              />
+            </div>
+          ) : (
+            <div className="absolute top-0 left-0 z-10 p-4 ml-16 mt-4 max-w-xl bg-white rounded shadow-lg max-h-[95%] overflow-y-auto">
               <Plan
                 roomId={roomId}
                 onStationSelect={handleStationSelect}
@@ -239,10 +306,7 @@ export const Map: React.FC<MapProps> = (props) => {
                 }
               />
             </div>
-          </div>
-        )}
-        <div className="absolute top-0 right-0 z-10 p-4 mr-4 mt-4 max-w-xs bg-white rounded shadow-lg">
-          <button onClick={logout}>ログアウト</button>
+          )}
         </div>
         <GoogleMap
           mapContainerStyle={containerStyle}
@@ -254,7 +318,8 @@ export const Map: React.FC<MapProps> = (props) => {
             fullscreenControl: false,
             mapTypeControl: true,
             mapTypeControlOptions: {
-              position: google.maps.ControlPosition.TOP_CENTER,
+              position: google.maps.ControlPosition.TOP_RIGHT,
+              style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
             },
           }}
           onLoad={(map) => {
